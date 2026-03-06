add3e30a2563d405ceb62cdf379952202761eca2
SasanDilantha <sasandilantha@gmail.com>
2026-03-06 19:54:34 +0530

    update: Add utility folder and functions

---
+++ Multi-Agent-Stock-Analyist/utils/utilities.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+def mask_api_key(key: str) -> str:
+    if len(key) <= 8:
+        return "****"
+    return key[:4] + "****" + key[-4:]
+
+def get_google_api_key() -> str:
+    google_api_key = os.environ.get("MODEL_API_KEY", "")
+    if not google_api_key:
+        raise ValueError("MODEL_API_KEY environment variable not set.")
+    return google_api_key
+
+def get_model_name() -> str:
+    model_name = os.environ.get("MODEL_NAME", "")
+    if not model_name:
+        raise ValueError("MODEL_NAME environment variable not set.")
+    return model_name
